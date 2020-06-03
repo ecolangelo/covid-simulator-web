ca5648f65f1866ee73e4dc210791c4bde8b1d91a
Eros <eros.colangelo@unicredit.eu>
2020-06-03 19:46:26 +0200

    first working version showing graph data

--- src/main/java/com/github/ecolangelo/covidsimulatorweb/CovidSimulatorService.java
+++ src/main/java/com/github/ecolangelo/covidsimulatorweb/CovidSimulatorService.java
@@ -1,29 +1,64 @@
 package com.github.ecolangelo.covidsimulatorweb;
 
+import com.github.ecolangelo.ReportGenerator;
 import com.github.ecolangelo.ingestion.CovidDataService;
 import com.github.ecolangelo.ingestion.CovidDataset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @RestController
 public class CovidSimulatorService {
 
+    private final Logger LOG = LoggerFactory.getLogger(CovidSimulatorService.class);
+
     @Autowired
     private CovidDataService covidDataService;
 
-    
+
+    private Map<LocalDate,SimulatorData> noexpirableCache = new ConcurrentHashMap<>();
 
 
     @GetMapping("/national")
     public SimulatorData getSimulation() throws Exception{
-        CovidDataset national = covidDataService.getNational();
+        LocalDate now = LocalDate.now();
+        if(noexpirableCache.get(now)!= null) {
+            return noexpirableCache.get(now);
+        }
 
+        CovidDataset national = covidDataService.getNational();
+        double[][] simulate = ReportGenerator.runLinearRegression(national.records().collect(Collectors.toList()), now, LOG::info);
+
+
+
+
+        String[] dates = new String[simulate.length];
+        double[] values = new double[simulate.length];
+
+
+        for(int i = 0;i<simulate.length;i++) {
+            double nDays = simulate[i][0]+1;
+            double estimateInfections = simulate[i][1];
+
+            LocalDate localDate = now.plusDays(i+1);
+            dates[i] = localDate.toString();
+            values[i] = estimateInfections;
+
+        }
         SimulatorData simulatorData = new SimulatorData();
 
+        simulatorData.setPlottyLineForInfections(new PlottyLineForInfections(dates, values));
+        simulatorData.setLastUpdated(now.toString());
 
+        simulatorData.setEstimatedDayOfEnding(dates[dates.length-1]);
 
 
         return simulatorData;
--- src/main/java/com/github/ecolangelo/covidsimulatorweb/CovidSimulatorWebApplication.java
+++ src/main/java/com/github/ecolangelo/covidsimulatorweb/CovidSimulatorWebApplication.java
@@ -1,13 +1,46 @@
 package com.github.ecolangelo.covidsimulatorweb;
 
+import com.github.ecolangelo.ingestion.CovidDataService;
+import com.github.ecolangelo.ingestion.HttpGithubService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
 
 @SpringBootApplication
 public class CovidSimulatorWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CovidSimulatorWebApplication.class, args);
+	}
+
+
+	@Bean
+	public CovidDataService covidDataService() {
+	    Properties p = new Properties();
+		try {
+			p.load(new FileInputStream("credentials"));
+			return new HttpGithubService(p.getProperty("user"),p.getProperty("password"));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/national").allowedOrigins("*");
+			}
+		};
 	}
 
 }
